c356433c1404a048c22dbcb6c66fd9d619be5b91
isamu <isamu@to-kyo.to>
2023-05-01 19:26:46 +0900

    update link

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -29,12 +29,12 @@ export default defineComponent({
   setup () {
     return {
       sharing: {
-        url: 'https://news.vuejs.org/issues/180',
-        title: 'Say hi to Vite! A brand new, extremely fast development setup for Vue.',
-        description: 'This week, I’d like to introduce you to "Vite", which means "Fast". It’s a brand new development setup created by Evan You.',
-        quote: 'The hot reload is so fast it\'s near instant. - Evan You',
-        hashtags: 'vuejs,vite,javascript',
-        twitterUser: 'youyuxi'
+        url: location.href,
+        title: 'chat.',
+        description: 'chat.',
+        quote: 'chat',
+        hashtags: 'chat',
+        twitterUser: 'snakajima'
       },
       networks: [
         { network: 'baidu', name: 'Baidu', icon: 'fas fah fa-lg fa-paw', color: '#2529d8' },
