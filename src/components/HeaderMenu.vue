1530c7b1b8e92767f49ec80652367f50f2531705
isamu <isamu@to-kyo.to>
2023-05-05 08:22:05 +0900

    add logo

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="nav fl">
-    <template v-if="isSignedIn">
-      <router-link :to="localizedUrl('/')">Home</router-link> |
-      <span @click="signout">Signout</span>
-    </template>
-    <template v-else>
-      <router-link :to="localizedUrl('/')">Home</router-link> |
-      <router-link :to="localizedUrl('/account')">Signin</router-link>
-    </template>
-  </div>
+<div id="nav fl">
+  <img :src="logo" class="w-12"  />
+  <template v-if="isSignedIn">
+    <router-link :to="localizedUrl('/')">Home</router-link> |
+    <span @click="signout">Signout</span>
+  </template>
+  <template v-else>
+    <router-link :to="localizedUrl('/')">Home</router-link> |
+    <router-link :to="localizedUrl('/account')">Signin</router-link>
+  </template>
+</div>
 </template>
 
 <script lang="ts">
@@ -17,11 +18,15 @@ import { useIsSignedIn } from "@/utils/utils";
 import { auth } from "@/utils/firebase";
 import { signOut } from "firebase/auth";
 
+// import Logo from "@/components/LogoMark.vue";
+import logo from "@/components/LogoMark.svg?raw";
+
 // import Languages from "./Languages.vue";
 
 export default defineComponent({
   components: {
-    //    Languages,
+    // Languages,
+    // Logo,
   },
   setup() {
     const isSignedIn = useIsSignedIn();
@@ -29,9 +34,11 @@ export default defineComponent({
     const signout = () => {
       signOut(auth);
     };
+    console.log(logo);
     return {
       isSignedIn,
       signout,
+      logo,
     };
   },
 });
