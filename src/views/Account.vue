ccf72ac32f010a8d473adc00cb1479e63cb6996e
isamu <isamu@to-kyo.to>
2023-05-07 05:22:32 +0900

    nickname

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,34 +1,52 @@
 <template>
   <div>
-    <h2>Account</h2>
+    <h2 class="font-bold text-white m-2">Account</h2>
     <div v-if="store.state.statistics">
       Today /{{ store.state.statistics.chatCounter }} Chat /
       {{ store.state.statistics.messageCounter }} Message
     </div>
-    {{ nickname }}
+
+    <div>
+      <div>
+        <input v-model="nickName" class="rounded-lg p-4 border-0" />
+      </div>
+      <div>
+        <button
+          class="m-2 rounded-lg border-2 p-2 font-bold text-white bg-blue-600"
+          @click="save"
+          >
+          保存
+        </button>
+      </div>
+      <div v-if="isSaved" class="font-bold text-white">
+        保存しました
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, watch, ref } from "vue";
-import { useUser } from "@/utils/utils";
+import { useUser, sleep } from "@/utils/utils";
 import { useStore } from "vuex";
 import { db } from "@/utils/firebase";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 
 export default defineComponent({
   setup() {
     const store = useStore();
     const user = useUser();
-    const nickname = ref("");
-
+    const nickName = ref("");
+    const isSaved = ref(false);
+    
     const path = computed(() => {
       return `users/${user.value?.uid}`;
     });
     const loadName = async () => {
       if (path.value) {
         const muser = await getDoc(doc(db, `${path.value}`));
-        nickname.value = (muser.data() || {}).nickName;
+        nickName.value = (muser.data() || {}).nickName;
       }
     };
     watch(path, async () => {
@@ -36,9 +54,21 @@ export default defineComponent({
     });
     loadName();
 
+    const save = async () => {
+      if (path.value) {
+        updateDoc(doc(db, `${path.value}`), {nickName: nickName.value});
+
+        isSaved.value = true;
+        await sleep(2);
+        isSaved.value = false;
+      }
+    };
+
     return {
       store,
-      nickname,
+      nickName,
+      save,
+      isSaved,
     };
   },
 });
